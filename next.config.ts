439a6f673f59740f5b5900761ce6fee9b818c9a9
Raul Martinez F <loggier@gmail.com>
2025-06-21 06:11:21 +0000

    fix

--- next.config.ts
+++ next.config.ts
@@ -4,8 +4,7 @@ const withPWA = require('next-pwa')({
   dest: 'public',
   register: true,
   skipWaiting: true,
-  disable: process.env.NODE_ENV === 'development',
-  buildExcludes: [/middleware-manifest.json$/]
+  disable: process.env.NODE_ENV === 'development'
 });
 const nextConfig = {
   /* config options here */
