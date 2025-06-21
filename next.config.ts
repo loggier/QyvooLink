296ff942bf861f118a40863aa09e58f8b3d69739
Raul Martinez F <loggier@gmail.com>
2025-06-21 01:39:03 +0000

    ajsute next

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,10 @@
 
 import type {NextConfig} from 'next';
-
+const withPWA = require('next-pwa')({
+  dest: 'public',
+  register: true,
+  skipWaiting: true,
+});
 const nextConfig: NextConfig = {
   /* config options here */
   typescript: {
@@ -27,4 +31,4 @@ const nextConfig: NextConfig = {
   },
 };
 
-export default nextConfig;
+export default withPWA(nextConfig);
