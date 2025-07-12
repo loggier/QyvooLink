340f7fa80263a5cb3376c9a402c36febd0764a15
Raul Martinez F <loggier@gmail.com>
2025-07-12 23:31:44 +0000

    I see this error with the app, reported by NextJS, please fix it. The er

--- src/next.config.ts
+++ src/next.config.ts
@@ -1,2 +1,52 @@
 
-// This file is intentionally left blank to avoid conflicts with the root next.config.ts
+
+import type { NextConfig } from 'next';
+
+/** @type {import('next').NextConfig} */
+const nextConfig: NextConfig = {
+  typescript: {
+    ignoreBuildErrors: true,
+  },
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'placehold.co',
+        port: '',
+        pathname: '/**',
+      },
+       {
+        protocol: 'https',
+        hostname: 'qyvoo.vemontech.com',
+        port: '',
+        pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'ui-avatars.com',
+        port: '',
+        pathname: '/**',
+      }
+    ],
+  },
+  webpack: (config, { isServer }) => {
+    if (!isServer) {
+      config.resolve.fallback = {
+        ...config.resolve.fallback,
+        stream: require.resolve('stream-browserify'),
+        crypto: require.resolve('crypto-browserify'),
+        buffer: require.resolve('buffer/'),
+        child_process: false,
+        async_hooks: false,
+        fs: false,
+        net: false,
+      };
+    }
+    return config;
+  },
+};
+
+export default nextConfig;
